d79c47a35d71d83012dc614af2c66cb8a038b762
koes_c <dktlfem@naver.com>
2025-12-11 16:23:34 +0900

    feat: UserEntity 생성 (2025/12/11 최민석)

---
+++ src/main/java/com/koesc/ci_cd_test_app/domain/user/entity/UserEntity.java
@@ -0,0 +1,67 @@
+package com.koesc.ci_cd_test_app.domain.user.entity;
+
+import com.koesc.ci_cd_test_app.domain.user.dto.UserRequestDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "user")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false, updatable = false)
+    private String username;
+
+    @Column(name = "password", nullable= false)
+    private String password;
+
+    @Column(name = "is_lock", nullable = false)
+    private Boolean isLock;
+
+    @Column(name = "is_social", nullable = false)
+    private Boolean isSocial;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "social_provider_type")
+    private SocialProviderType socialProviderType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_type", nullable = false)
+    private UserRoleType roleType;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "email")
+    private String email;
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+
+    // @Setter 가 없기 때문에 커스텀한 수정 메서드를 만들어야 한다.
+    public void updateUser(UserRequestDTO dto) {
+        this.email = dto.getEmail();
+        this.nickname = dto.getNickname();
+    }
+}
