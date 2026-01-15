1fa718436a62f87a6aba1b2eff455d1869de3cf1
koes_c <dktlfem@naver.com>
2026-01-15 21:12:40 +0900

    refactor(main): scanBasePackages를 명시하여 모든 하위 패키지의 빈(Bean) 스캔을 강제

--- src/main/java/com/koesc/ci_cd_test_app/CiCdTestAppApplication.java
+++ src/main/java/com/koesc/ci_cd_test_app/CiCdTestAppApplication.java
@@ -3,7 +3,8 @@ package com.koesc.ci_cd_test_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// scanBasePackages를 명시하여 모든 하위 패키지의 빈(Bean) 스캔을 강제합니다.
+@SpringBootApplication(scanBasePackages = "com.koesc.ci_cd_test_app")
 public class CiCdTestAppApplication {
 
 	public static void main(String[] args) {
