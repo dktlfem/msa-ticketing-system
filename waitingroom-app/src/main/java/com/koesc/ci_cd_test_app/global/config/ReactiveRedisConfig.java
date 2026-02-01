186872029c224cb98bfa335ee210856124feb5c1
koes_c <dktlfem@naver.com>
2026-02-02 01:42:24 +0900

    fix: ReactiveRedisConfig 오타 수정 및 빈 충돌 해결 (@Primary)

--- waitingroom-app/src/main/java/com/koesc/ci_cd_test_app/global/config/ReactiveRedisConfig.java
+++ waitingroom-app/src/main/java/com/koesc/ci_cd_test_app/global/config/ReactiveRedisConfig.java
@@ -3,6 +3,7 @@ package com.koesc.ci_cd_test_app.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -23,6 +24,7 @@ public class ReactiveRedisConfig {
     private String password;
 
     @Bean
+    @Primary
     public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
         config.setPassword(password);
